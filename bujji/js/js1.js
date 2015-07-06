4aef2e63424a2a12d8288f162abec6c08f8c2541
bujjibabu <bujji404@gmail.com>
2015-07-06 07:25:19 +0530

    javascript introduction

---
+++ bujji/js/js1.js
@@ -0,0 +1,116 @@
+    //alert(1+2);
+    //code..
+
+    /* var x = 10;
+    var y = "bujji" or 'bujji';
+    var z= {};
+    var a; */
+    //var A;
+    //int x =4
+
+    var x = 4;
+    var y = 5;
+   // debugger;
+    var a = 12.3345;
+    var z = x+y;
+    //alert(z);
+    console.log(z);
+
+/*    js data types
+
+    .numbers
+    	var x = 4;
+	    var y = 5;
+	    var a = 12.3345;
+
+    strings
+    var x = 'bujji';
+    or
+    var x = "bujji";
+
+	boolean
+	var x = true;
+	var y = false;*/
+
+var fanme ="Bujji";
+var lanme ="Babu";
+
+var name = fanme + lanme;
+
+//alert(name);
+
+//operations
+/*var num1 = 10;
+var nmu2 = 20;
+var num3 = num1++;
+var num4 = num1--;
+var add = num1+nmu2;
+var mul = num1*nmu2;
+var div = num1/nmu2;*/
+/*
+comparison operations
+
+var x = 10;
+var y = 4+6;
+	x==y --> true
+	x != y  --> false
+	x>y ->false
+	x<y -> false
+	x<=y -->true
+	x>=y  -->true
+
+
+local operators
+
+a=10;
+b= false;
+
+a&&b -->logicalAND -->f
+
+a||b ->logicalOR ->t
+
+ !() ->not
+
+!(a&&b) -t
+!(a||b) -f
+
+
+
+username
+
+password 
+
+if(username && password)
+{
+
+}
+
+assignment operators
+ =
+ +=
+ var x= 10;
+ 	 x+=1;
+ 	 +=
+ 	 -=
+ 	 *=
+ 	 /=
+ 	 %=
+*/
+
+
+/*trenary operator
+
+
+(condition) ? trustate : falsesta
+
+var x = 10;
+var y =2+8;
+
+(x==y) ? 'Both are equal' : 'not '*/
+debugger;
+
+var myDate = new Date();
+
+
+
+
