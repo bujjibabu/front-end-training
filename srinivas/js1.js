e1477656194437988dd001adc230940596231b4c
Rajalasrinivasulu <rsreddy.engg@gmail.com>
2015-07-07 20:25:57 -0500

    Java Script Practice..

---
+++ srinivas/js1.js
@@ -0,0 +1,15 @@
+
+	var x=10;
+	var y=30;
+	var add=x+y;
+	document.write("addition of two number is "+add+" ");
+	 var sub=x-y;
+	document.write("subtraction of two number is "+sub+" ");
+
+	var mul=x*y;
+	document.write("Multiplication of two number is "+mul+" ");
+
+	var div=x/y;
+	document.write("Division  of two number is "+div+" ");
+	var modul=x%y;
+	document.write("Modular  of two number is "+modul+" ");
